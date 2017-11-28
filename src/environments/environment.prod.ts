41fb25957dca3e64adcf6d905df84f49820c17ac
Mschoot1 <marnix_vd_schoot@hotmail.com>
2017-11-28 12:47:23 +0100

    / char removed from env prod serverurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverUrl: 'https://node-mongodb-server-project.herokuapp.com/api/v1/'
+  serverUrl: 'https://node-mongodb-server-project.herokuapp.com/api/v1'
 };
