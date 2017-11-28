6ed6685e8f931eb9b5925e00775c082e86e1806a
Mschoot1 <marnix_vd_schoot@hotmail.com>
2017-11-28 15:00:41 +0100

    serverUrl edited

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,15 +1,44 @@
-import { Ingredient } from '../shared/ingredient.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class Recipe {
-  public name: string;
-  public description: string;
-  public imagePath: string;
-  public ingredients: Ingredient[];
+  private _name: string;
+  private _description: string;
+  private _imagePath: string;
+  private _ingredients: Ingredient[];
 
-  constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
-    this.name = name;
-    this.description = desc;
-    this.imagePath = imagePath;
-    this.ingredients = ingredients;
+  constructor(values: Object = {}) {
+    Object.assign(this, values);
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public set name(n: string) {
+    this._name = n;
+  }
+
+  public get ingredients(): Ingredient[] {
+    return this._ingredients;
+  }
+
+  public set ingredients(value: Ingredient[]) {
+    this._ingredients = value;
+  }
+
+  public get imagePath(): string {
+    return this._imagePath;
+  }
+
+  public set imagePath(value: string) {
+    this._imagePath = value;
+  }
+
+  public get description(): string {
+    return this._description;
+  }
+
+  public set description(value: string) {
+    this._description = value;
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverUrl: 'https://node-mongodb-server-project.herokuapp.com/api/v1'
+  serverUrl: 'https://node-server-users.herokuapp.com//api/v1'
 };
